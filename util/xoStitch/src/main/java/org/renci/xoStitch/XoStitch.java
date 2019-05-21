17dc6f5a93e2f3bae828e35c91860b232eb41d1b
paul-ruth <paul.ruth@gmail.com>
2019-05-21 14:36:34 -0400

    added ability to hop at an exogeni site

--- util/xoStitch/src/main/java/org/renci/xoStitch/XoStitch.java
+++ util/xoStitch/src/main/java/org/renci/xoStitch/XoStitch.java
@@ -16,7 +16,9 @@ import org.renci.ahab.libtransport.util.SSHAccessTokenFileFactory;
 import org.renci.ahab.libtransport.xmlrpc.XMLRPCProxyFactory;
 
 import java.net.URL;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.Map;
 
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.Option;
@@ -57,6 +59,9 @@ public class XoStitch
   static String sp1_label = null;
   static String sp2_label = null;
 
+  static String hop = null;
+
+  static Map<String, String> domains;
 
 
   static ISliceTransportAPIv1 sliceProxy;
@@ -67,6 +72,29 @@ public class XoStitch
       KNOWN_STITCHPORTS = new HashMap<String, String>();
       KNOWN_STITCHPORTS.put("uc", "http://geni-orca.renci.org/owl/ion.rdf#AL2S/Chameleon/Cisco/6509/GigabitEthernet/1/1");
       KNOWN_STITCHPORTS.put("tacc", "http://geni-orca.renci.org/owl/ion.rdf#AL2S/TACC/Cisco/6509/TenGigabitEthernet/1/1");
+
+
+    		domains = new HashMap<String, String>();
+    		domains.put("rci", "RENCI (Chapel Hill, NC USA) XO Rack");
+    		domains.put("bbn", "BBN/GPO (Boston, MA USA) XO Rack");
+    		domains.put("fiu","FIU (Miami, FL USA) XO Rack");
+    		domains.put("uh","UH (Houston, TX USA) XO Rack");
+    		domains.put("uva","UvA (Amsterdam, The Netherlands) XO Rack");
+    		domains.put("ufl","UFL (Gainesville, FL USA) XO Rack");
+    		domains.put("uaf","UAF (Fairbanks, AK, USA) XO Rack");
+    		domains.put("sl","SL (Chicago, IL USA) XO Rack");
+    		domains.put("wvn","WVN (UCS-B series rack in Morgantown, WV, USA)");
+    		domains.put("tamu","TAMU (College Station, TX, USA) XO Rack");
+    		domains.put("umass","UMass (UMass Amherst, MA, USA) XO Rack");
+    		domains.put("wsu","WSU (Detroit, MI, USA) XO Rack");
+    		domains.put("psc","PSC (Pittsburgh, TX, USA) XO Rack");
+    		domains.put("unf","UNF (Jacksonville, FL) XO Rack");
+    		domains.put("geu","GWU (Washington DC,  USA) XO Rack");
+    		domains.put("ciena","CIENA (Ottawa,  CA) XO Rack");
+    		domains.put("ciena2","CIENA2 (Hanover, MD) XO Rack");
+
+
+
 
         System.out.println( "Creating a simple Slice!" );
 
@@ -98,6 +126,7 @@ public class XoStitch
         //Option option_keyLocation = Option.builder("k").required(false).argName("keyLocation").hasArg().desc("keyLocation").build();
 
         Option option_controllerUrl= Option.builder("u").required(false).argName("controllerUrl").hasArg().desc("controllerUrl").build();
+        Option option_hop= Option.builder("hop").required(false).argName("hop").hasArg().desc("hop").build();
 
         Option option_sliceNane= Option.builder("n").required(false).argName("sliceNane").hasArg().desc("sliceName").build();
         Option option_passphrase= Option.builder("p").required(false).argName("passphrase").hasArg().desc("passphrase").build();
@@ -118,7 +147,7 @@ public class XoStitch
         options.addOption(option_url2);
         options.addOption(option_vlan1);
         options.addOption(option_vlan2);
-
+        options.addOption(option_hop);
 
         options.addOption(option_certLocation);
         //options.addOption(option_keyLocation);
@@ -147,11 +176,20 @@ public class XoStitch
               System.exit(1);
           }
 
+
+
           //Set cert passphrase
           if (commandLine.hasOption("p")){
               passphrase = commandLine.getOptionValue("p");
           } else {
               passphrase = "";
+          }
+
+          //Set hop
+          String hopStr = "";
+          if (commandLine.hasOption("hop")){
+              hopStr = commandLine.getOptionValue("hop");
+              hop = domains.get(hopStr);
           }
 
           //optionally set slice name
@@ -248,7 +286,11 @@ public class XoStitch
 
         //Call command
         if (command.equals("create")){
-          XoStitch.createSlice(certLocation,keyLocation,controllerUrl,sliceName);
+          if (hop != null) {
+            XoStitch.createHopSlice(hop,certLocation,keyLocation,controllerUrl,sliceName);
+          } else {
+            XoStitch.createSlice(certLocation,keyLocation,controllerUrl,sliceName);
+          }
         } else if (command.equals("delete")){
           XoStitch.deleteSlice(sliceName);
         }else if (command.equals("status")){
@@ -300,6 +342,56 @@ public class XoStitch
       }
       return;
     }
+
+    public static void createHopSlice(String hopSite, String certLocation, String keyLocation, String controllerUrl, String sliceName ) throws Exception{
+      System.out.println("createHopSlice: " + hopSite + ", " + certLocation + ", " + keyLocation + ", " + controllerUrl + ", " + sliceName);
+      System.out.println("sp1: " + sp1_label + ", " + sp1_url);
+      System.out.println("sp2: " + sp2_label + ", " + sp2_url);
+
+      Slice slice = Slice.create(sliceProxy,sctx,sliceName);
+
+      String imageShortname="centos7-v1.0.4-openvswitch";
+      String imageURL ="http://geni-images.renci.org/images/standard/centos/centos7.4-v1.0.4-openvswitch/centos7.4-v1.0.4-openvswitch.xml";
+      String imageHash ="77094c373a787c0d2ff5594b26088a3a716f5c2f";
+      String domain=hopSite;
+      String nodeType="XO Medium";
+      String postBootScript=" #!/bin/bash \n " +
+                              " BRIDGE=br0 \n " +
+                              " ovs-vsctl add-br br0   \n " +
+                              " while true; do \n " +
+                              "  echo looping  \n " +
+                              "  for iface in `ifconfig  | grep flags | awk -F\":\"  '{print $1}'`; do  \n " +
+                              "     echo $iface  \n " +
+                              "     ip address show dev $iface  | grep inet  | grep $iface  \n " +
+                              "     if [ $? -eq 0 ]; then  \n " +
+                              "        echo Skipping $iface     \n " +
+                              "     else  \n " +
+                              "        echo adding $iface  \n " +
+                              "        ovs-vsctl add-port br0 $iface  \n " +
+                              "     fi  \n " +
+                              "  done  \n " +
+                              "  sleep 5  \n " +
+                              " done  \n ";
+
+
+
+
+
+      ComputeNode   hopNode = slice.addComputeNode("hop");
+      hopNode.setImage(imageURL,imageHash,imageShortname);
+      hopNode.setNodeType(nodeType);
+      hopNode.setDomain(domain);
+      hopNode.setPostBootScript(postBootScript);
+
+      StitchPort sp1 = slice.addStitchPort("sp1",sp1_label,sp1_url,100000000);
+      StitchPort sp2 = slice.addStitchPort("sp2",sp2_label,sp2_url,100000000);
+
+      hopNode.stitch(sp1);
+      hopNode.stitch(sp2);
+      //System.out.println(slice.getRequest());
+      slice.commit();
+    }
+
 
     public static void createSlice(String certLocation, String keyLocation, String controllerUrl, String sliceName ) throws Exception{
       System.out.println("createSlice: " + certLocation + ", " + keyLocation + ", " + controllerUrl + ", " + sliceName);
